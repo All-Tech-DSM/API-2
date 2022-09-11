5202a2a878057e14cabc933544cf355bee36c8b1
Desduh <eduardofalandess@gmail.com>
2022-09-11 20:54:33 -0300

    correcao de erros no banco de dados

--- src/script.sql
+++ src/script.sql
@@ -6,18 +6,26 @@ fun_cod int auto_increment primary key,
 nome varchar(70) not null, 
 email varchar(40) not null unique key, 
 cpf varchar(11)  not null unique key, 
-nascimento date, 
-telefone varchar(11), 
-celular varchar(11),
+nas_data date, 
+tel_fixo varchar(11), 
+tel_celular varchar(11),
 cep varchar(8),
 numero int(6),
 rua varchar(50),
 bairro varchar(50),
-cidade varchar(50));
+cidade varchar(50),
+estado varchar(50),
+complemento varchar(50));
+
+create table emprego(
+emp_cod int auto_increment primary key,
+fun_cod int not null,
+foreign key(fun_cod) REFERENCES funcionario (fun_cod),
+esc_cod int not null,
+foreign key(esc_cod) REFERENCES escolas (esc_cod)
+);
 
 create table escolas(
 esc_cod int auto_increment primary key,
-fun_cod int not null,
-foreign key(fun_cod) REFERENCES funcionario (fun_cod),
 escola varchar(100) not null 
 );
