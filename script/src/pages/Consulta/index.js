bb2e66937f25ac1ff614b948348869bb61cccd7f
Desduh <eduardofalandess@gmail.com>
2022-09-17 20:52:02 -0300

    adicao da visualizacao

--- script/src/pages/Consulta/index.js
+++ script/src/pages/Consulta/index.js
@@ -6,14 +6,19 @@ import Axios from "axios";
 
 function Consulta() {
   const [dados, setDados] = useState([]);
+  const [dado, setDado] = useState([]);
 
   Axios.get("http://localhost:3001/pdf_inf").then((response) => {
-    console.log(response.data);
+    setDado(response.data);
   });
+  console.log(dado)
+  console.log()
+
+  // var x = [['"rodrigo garcia"', 'eduardofalandess@gmail.com', ['http://www.imprensaoficial.com.br/DO/GatewayPDF.aspx?link=%2f2022%2fexecutivo+secao+i%2fsetembro%2f17%2fpag_0001_05789c6355b2032cb54a01f77b6c114d.pdf&pagina=1&data=17/09/2022&caderno=ExecutivoI&paginaordenacao=100001', 'http://www.imprensaoficial.com.br/DO/GatewayPDF.aspx?link=%2f2022%2fexecutivo+secao+i%2fsetembro%2f17%2fpag_0107_719a6d875de990dd38fcc8e4abb9d7a3.pdf&pagina=107&data=17/09/2022&caderno=ExecutivoI&paginaordenacao=100107', 'http://www.imprensaoficial.com.br/DO/GatewayPDF.aspx?link=%2f2022%2fexecutivo+secao+i%2fsetembro%2f17%2fpag_0088_aadf43b1c6df8c3075e8cb177747c15f.pdf&pagina=88&data=17/09/2022&caderno=ExecutivoI&paginaordenacao=100088', 'http://www.imprensaoficial.com.br/DO/GatewayPDF.aspx?link=%2f2022%2fexecutivo+secao+ii%2fsetembro%2f17%2fpag_0001_c754e4022455326cde557992404bb970.pdf&pagina=1&data=17/09/2022&caderno=ExecutivoII&paginaordenacao=100001']]]
 
   return (
     <div>
-      {dados.lenght === 0 ? (
+      {dados.lenght === true ? (
         <div className='none'>
           <span>Nenhum dado encontrado...</span>
         </div>
@@ -32,45 +37,7 @@ function Consulta() {
 
               <tbody>
                 
-                    <tr>
-                      <td data-label="Nome">Amanda Vieira de Oliveira</td>
-                      <td data-label="Email">amandavieiradeoliveira@gmail.com</td>
-                      <td data-label="Consulta">
-                        <button>
-                          <FiDownload size={25} />
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td data-label="Nome">Bianca Oliveira Vieira</td>
-                      <td data-label="Email">biancaoliveiravieira@gmail.com</td>
-                      <td data-label="Consulta">
-                        <button>
-                          <FiDownload size={25} />
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td data-label="Nome">Camila Vieira de Oliveira</td>
-                      <td data-label="Email">amandavieiradeoliveira@gmail.com</td>
-                      <td data-label="Consulta">
-                        <button>
-                          <FiDownload size={25} />
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td data-label="Nome">Damaris Oliveira Vieira</td>
-                      <td data-label="Email">biancaoliveiravieira@gmail.com</td>
-                      <td data-label="Consulta">
-                        <button>
-                          <FiDownload size={25} />
-                        </button>
-                      </td>
-                    </tr>
+                
      
               </tbody>
             </table>
