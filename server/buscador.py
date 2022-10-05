d19fcf34d2d2517f569ae39193e4cc0627f56949
Júlia Gayotto <juliagayotto@gmail.com>
2022-10-05 09:55:59 -0300

    atualizacao do buscador e coletor

--- server/buscador.py
+++ server/buscador.py
@@ -3,8 +3,10 @@ from datetime import date
 from coletor import coletor
 import requests
 import os
-def func():
-    con = MySQLdb.connect( user="root", password="admin", db="cadastro")
+import shutil
+from PyPDF2 import PdfFileReader, PdfFileMerger
+def busca():
+    con = MySQLdb.connect( user="root", password="fatec", db="cadastro")
     cur = con.cursor()
     cur.execute('SELECT nome, email FROM funcionario ORDER BY nome;')
     feedback=cur.fetchall()
@@ -14,10 +16,9 @@ def func():
         buffer.append('"'+feedback[i][0].lower()+'"')
         buffer.append(feedback[i][1])
         nomes.append(buffer)
-    data = '2022-09-17'#str(date.today())
+    data = '2022-09-17' #str(date.today())
     datatext = '17/09/2022' #date.today()
     data1 = data.replace('-', '')
-    data2 = data.replace('-', '.')
     data3 = datatext #.strftime( '%d/%m/%Y' )
     data3parte2 = data3.replace('/', '.')
     data4 = data.replace('/', '%2f')
@@ -29,33 +30,45 @@ def func():
         nome2 = nomes[n][0].replace(' ', '%2b')
 
         url = (f'http://www.imprensaoficial.com.br/DO/BuscaDO2001Resultado_11_3.aspx?filtropalavraschave=%22{nome}%22&f=xhitlist&xhitlist_vpc=first&xhitlist_x=Advanced&xhitlist_q=%5bfield+%27dc%3adatapubl%27%3a%3e%3d{data3parte2}%3c%3d{data3parte2}%5d({nome2})&filtrogrupos=Cidade+de+SP%2c+Executivo+&xhitlist_mh=9999&filtrodatafimsalvar={data1}&filtroperiodo={data4}+a+{data4}&filtrodatainiciosalvar={data1}&filtrogrupossalvar=Cidade+de+SP%2c+Executivo+&xhitlist_hc=%5bXML%5d%5bKwic%2c3%5d&xhitlist_vps=15&filtrotodosgrupos=False&xhitlist_d=Cidade+de+SP%2c+Executivo+&filtrotipopalavraschavesalvar=UP&xhitlist_s=&xhitlist_sel=title%3bField%3adc%3atamanho%3bField%3adc%3adatapubl%3bField%3adc%3acaderno%3bitem-bookmark%3bhit-context&xhitlist_xsl=xhitlist.xsl')
-        res.append(coletor(url, nomes[n]))
-        res.append('*')
-
-    dir = (os.getcwd().strip('server')) + 'PDF'
-    for f in os.listdir(dir):
-        os.remove(os.path.join(dir, f))
-
-    for lit in res:
-        #print(links)
-        for l in range(2, len(lit)):
-            if len(lit) <3:
-                break
-            r = requests.get(lit[l], stream = True)
-            with open(dir+'/' + lit[0][1:-1] + str(l-1)+'.pdf', "wb") as f:
-                f.write(r.content)
-
+        pessoa = []
+        pessoa.append(nomes[n][0])
+        pessoa.append(nomes[n][1])
+        pessoa.append(coletor(url))
+        res.append(pessoa)
 
     filtro = res
     res = []
     for i in filtro:
-        if i == '*':
-            res.append(i)
-        if len(i) > 2 :
+        if i[2] != [] :
             i[0] = i[0].replace('"', '')
             res.append(i)
-    res.pop()
-    res.pop(0)
-    print(res)
 
-func()
+
+    dir = (os.getcwd().strip('server')) + 'PDF' 
+    for f in os.listdir(dir):
+        shutil.rmtree(os.path.join(dir, f))
+    for p in res:
+        pdf_n = 1
+        for l in p[2]:
+            # if len(lit) <3:
+            #     break
+            r = requests.get(l, stream = True)
+            if not os.path.exists(f'{dir}/{p[0]}'):
+                os.makedirs(f'{dir}/{p[0]}')
+            with open(dir+'/' + p[0] + '/' + p[0] + str(pdf_n)+'.pdf', "wb") as f:
+                f.write(r.content)
+            pdf_n = pdf_n +1
+    pdf_files = [f for f in os.listdir(dir)]
+    merger = PdfFileMerger()
+    for i in pdf_files:
+        dir2 = (os.getcwd().strip('server')) + 'PDF/' + f'{i}'
+        pdf_files2 = [f for f in os.listdir(dir2)]
+        merger = PdfFileMerger()
+        for n in pdf_files2:
+            merger.append(PdfFileReader(os.path.join(dir2, n), "rb"))
+        for a in pdf_files2:
+            os.remove(dir2+'/'+a)
+        merger.write(os.path.join(dir2, "merger.pdf"))
+    return res
+
+busca()
--- server/coletor.py
+++ server/coletor.py
@@ -1,29 +1,19 @@
-
 import requests
 
-def coletor(url, info):
+def coletor(url):
+    def raspa(string):
+        comeco = string.find(com_ex)
+        if comeco == -1: return -1
+        string = string[comeco+len(com_ex):]
+        fim = string.find(fin_ex)
+        lista_link.append('http://www.imprensaoficial.com.br/DO/GatewayPDF' + string[5:fim].replace(' ', ''))
+        return raspa(string[fim:])
     r = requests.get(url)
     start = r.text.find('<div class="card shadow-sm resultadoBuscaItem">')
     end = r.text.find('<div class="rodape">')
     recorte = r.text[start:end]
-    final = 0
     com_ex = '<a class="bg-light text-dark" style="text-decoration: none;" href="/DO/BuscaDO2001Documento'
-    fin_ex = len('" target="_blank">')
-    res = [info[0], info[1]]
-    c = 0
-    while True:
-        começo = recorte.find(com_ex) + len(com_ex) + 5
-        if começo == 95:
-            break
-        for k in recorte[começo:]:
-            c+= 1
-            if k == '>':
-                final = começo+c
-                res.append(('http://www.imprensaoficial.com.br/DO/GatewayPDF' + recorte[começo:final-fin_ex]).replace(' ', ''))
-                recorte = recorte[final+1:]
-                c = 0
-                break
-    return res
-
-#a função recebe o nome da pessoa que esta sendo pesquisada apenas por referencia pois a lista retornada 
-#tera o primeiro elemento como o nome assim organizando os links junto com o nome procurado
+    fin_ex = '" target="_blank">'
+    lista_link = []
+    raspa(recorte)
+    return lista_link
