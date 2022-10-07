0a704048498bb10d5c1ee5f0edff2014b9e97652
LucasFrancaRegistro <lucascumuru@gmail.com>
2022-10-06 22:26:01 -0300

    trechos exibidos no front

--- script/src/pages/Dados/index.js
+++ script/src/pages/Dados/index.js
@@ -6,7 +6,6 @@ import Axios from "axios";
 
 function Dados() {
   var {id}=useParams();
-  //console.log(id)
   const [listo, setList] = useState([]);
 
   useEffect(() => {
@@ -15,9 +14,7 @@ function Dados() {
       setList(resp.data);
     });
   }, [])
-  console.log('repeticao')
   const [dado,setDado]=useState([]);
-  console.log(dado.length)
   if(dado.length == 0){
     for(var k = 0; listo.length > k; ++k){
       if(listo[k][1] == id){
@@ -26,29 +23,29 @@ function Dados() {
       } 
     }
   }
-  
+  console.log(dado[3])
 
 
 
 
 return (
   <div>
-            <div>
-                   <p id='dadodia'>
-                     Nome: {dado[0]} <br/>
-                     Email: {dado[2]}
-                   </p>
-            
-                    <div className='split'>
-                       <div className='pdf'>
-                         <div>pdf file will render here</div>
-                       </div>
-            
-                       <div className='text'>
-                         <p type='text'>{dado[-1]}</p>
-                       </div>
-                    </div>
-                 </div>
+    <div>
+      <p id='dadodia'>
+        Nome: {dado[0]} <br/>
+        Email: {dado[2]}
+      </p>
+
+      <div className='split'>
+          <div className='pdf'>
+            <div>pdf file will render here</div>
+          </div>
+
+          <div className='text'>
+            <p type='text'>{dado[3]}</p>
+          </div>
+      </div>
+    </div>
   </div>
 );
         }
