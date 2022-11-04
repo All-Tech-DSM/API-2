50d7c14113676f058fca8ba6ed77725e4abac303
LucasFrancaRegistro <101057737+LucasFrancaRegistro@users.noreply.github.com>
2022-11-04 10:20:21 -0300

    limpeza de codigo

--- server/app.py
+++ server/app.py
@@ -13,28 +13,6 @@ def associados():
     ls = data['array'] 
     result = busca()
     return json.dumps({"result":result})
-
-# @app.route('/trechos', methods = ['POST'])
-# # #[[nome, id]]
-# def trechos():
-#     print('trechos')
-#     data = request.get_json()
-#     nome = data['array']
-#     result = []
-#     for n in nome:
-#         pessoa = []
-#         pessoa.append(n[0])
-#         pessoa.append(n[1])
-#         pessoa.append(n[2])
-#         pessoa.append([])
-#         pessoa.append(leitor(n[0]))
-#         result.append(pessoa)
-#     return json.dumps({"result":result})
-    
-
-#[[id,nome,email,'pdf',trecho]]
-
-
    
 if __name__ == "__main__": 
     app.run(port=5000)
